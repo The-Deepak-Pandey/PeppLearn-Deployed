d197d39a2eecbe28de54f88d49672fb561f570b6
The-Deepak-Pandey <deepak.pandey@students.iiit.ac.in>
2025-01-18 15:34:52 +0530

    created file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from './components/ui/button'
+import Login from './pages/Login'
+
+function App() {
+
+  return (
+    <main>
+      <Button>Let's build lms</Button>
+      <Login />
+    </main>
+  )
+}
+
+export default App
